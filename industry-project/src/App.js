e772e20e30684e330e120af8d73cebd8d896032f
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-08-24 22:47:17 -0400

    updated onboarding component with different input button options

--- industry-project/src/App.js
+++ industry-project/src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Onboarding from "./components/Onboarding/Onboarding";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<Onboarding />} />
         <Route path="/compare" />
         <Route path="/details" />
       </Routes>
--- industry-project/src/components/Onboarding/Onboarding.js
+++ industry-project/src/components/Onboarding/Onboarding.js
@@ -11,7 +11,37 @@ function Onboarding() {
         </p>
         <input type="submit" value="option 1" />
         <input type="text" value="option 1" />
+
         <button>option 1</button>
+        {/* form option with radio buttons*/}
+        <form>
+          <label>
+            <input type="radio" name="answer" value="option1" />
+            Option 1
+          </label>
+          <label>
+            <input type="radio" name="answer" value="option2" />
+            Option 2
+          </label>
+          <label>
+            <input type="radio" name="answer" value="option3" />
+            Option 3
+          </label>
+          <button type="submit">Next</button>
+        </form>
+
+        {/* button option */}
+        <button type="button" class="option-button" data-value="option1">
+          Option 1
+        </button>
+        <button type="button" class="option-button" data-value="option2">
+          Option 2
+        </button>
+        <button type="button" class="option-button" data-value="option3">
+          Option 3
+        </button>
+
+        {/* dropdown menu option */}
         <label for="question">Select the correct answer:</label>
         <select id="question" name="question">
           <option value="option1">Option 1</option>
