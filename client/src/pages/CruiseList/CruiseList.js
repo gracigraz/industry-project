6fccbbc30bd2ff9f9db1fde0e326f8723c40e08f
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-25 10:41:33 -0400

    Fixing cruise list

--- client/src/pages/CruiseList/CruiseList.js
+++ client/src/pages/CruiseList/CruiseList.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import './CruiseList.scss'
 import cruiseList from '../../data/cruise-list.json'
 import { Link } from 'react-router-dom';
@@ -13,7 +12,8 @@ function CruiseList() {
             </section>
             {cruiseList.map((selectedCruise) => {
                 return (
-                    <Link className='cruise-list__link' to={'/cruise-details/' + selectedCruise.id}><section key={selectedCruise.id} className='cruise-list__section'>
+                    <div key={selectedCruise.id} >
+                    <Link className='cruise-list__link' to={'/cruise-details/' + selectedCruise.id}><section className='cruise-list__section'>
                         <div className='cruise-list__top-container'>
                             <img className='cruise-list__cruise-photo' src={`http://192.168.3.71:8080/images/bahamas-${selectedCruise.id}`} alt='test' />
                         </div>
@@ -25,61 +25,11 @@ function CruiseList() {
                         </div>
                     </section>
                     </Link>
+                    </div>
                 )
             })}
         </div>
     )
-=======
-import "./CruiseList.scss";
-import cocoCay from "../../assets/images/coco-cay.jpg";
-import cruiseList from "../../data/cruise-list.json";
-import { Link } from "react-router-dom";
-
-function CruiseList() {
-  return (
-    <div className="cruise-list">
-      <h1 className="cruise-list__h1">Cruise List</h1>
-      {cruiseList.map((selectedCruise) => {
-        return (
-          <Link
-            className="cruise-list__link"
-            to={"/cruise-details/" + selectedCruise.id}
-          >
-            <section key={selectedCruise.id} className="cruise-list__section">
-              <div className="cruise-list__top-container">
-                <img
-                  className="cruise-list__cruise-photo"
-                  src={cocoCay}
-                  alt="test"
-                />
-              </div>
-              <div className="cruise-list__bottom-container">
-                <h3 className="cruise-list__h3">
-                  <span className="cruise-list__bold">Boat name:</span>{" "}
-                  {selectedCruise.boat_name}
-                </h3>
-                <h3 className="cruise-list__h3">
-                  <span className="cruise-list__bold">
-                    Kids & family friendly:
-                  </span>{" "}
-                  {selectedCruise.family_friendly}
-                </h3>
-                <h3 className="cruise-list__h3">
-                  <span className="cruise-list__bold">Next departure:</span>{" "}
-                  {selectedCruise.next_departure}
-                </h3>
-                <h3 className="cruise-list__h3">
-                  <span className="cruise-list__bold">Port:</span>{" "}
-                  {selectedCruise.port}
-                </h3>
-              </div>
-            </section>
-          </Link>
-        );
-      })}
-    </div>
-  );
->>>>>>> Stashed changes
 }
 
 export default CruiseList;
