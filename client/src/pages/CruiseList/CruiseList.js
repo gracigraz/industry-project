69b6fba778d8a607ecd8e26b0aed5d401543ea24
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-25 08:32:38 -0400

    dividing client and server

--- client/src/pages/CruiseList/CruiseList.js
+++ client/src/pages/CruiseList/CruiseList.js
@@ -10,10 +10,10 @@ function CruiseList () {
             <h1 className='cruise-list__h1'>Cruise List</h1>
             {cruiseList.map((selectedCruise) => {
                 return (<section key={selectedCruise.id} className='cruise-list__section'>
-                <div className='cruise-list__left-container'>
+                <div className='cruise-list__top-container'>
                     <img className='cruise-list__cruise-photo' src={cocoCay} alt='test' />
                 </div>
-                <div className='cruise-list__right-container'>
+                <div className='cruise-list__bottom-container'>
                 <h3 className='cruise-list__h3'>Boat name: {selectedCruise.boat_name}</h3>
                 <h3 className='cruise-list__h3'>Kids & family friendly: {selectedCruise.family_friendly}</h3>
                 <h3 className='cruise-list__h3'>Next departure: {selectedCruise.next_departure}</h3>
