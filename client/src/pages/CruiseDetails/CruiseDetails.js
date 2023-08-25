24148c8aafa664d83ce03bbc341244f7f08af6de
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-25 12:01:03 -0400

    more tweaks cruise details and list

--- client/src/pages/CruiseDetails/CruiseDetails.js
+++ client/src/pages/CruiseDetails/CruiseDetails.js
@@ -33,6 +33,10 @@ function CruiseDetails() {
 
     }, [selectedCruise]);
 
+    function handleBackClick() {
+        window.location.replace('/cruise-list')
+    }
+
     if (selectedCruise == null) {
         return (<>
             <h1>Loading...</h1>
@@ -40,17 +44,20 @@ function CruiseDetails() {
     }
     else {
         return (
-            <main className='cruise-details'>
-                <img className='cruise-details__hero' src={cruiseImage} alt='Cruise ship you are seeing the details for' />
-                <h1 className='cruise-details__h1'>Cruise details</h1>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Destination:</span> {selectedCruise.destination}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Boat name: </span> {selectedCruise.boat_name}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Days at sea:</span> {selectedCruise.days_at_sea}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Capacity:</span> {selectedCruise.capacity}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Kids & family friendly:</span> {selectedCruise.family_friendly}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Last cruise reparation:</span> {selectedCruise.last_cruise_reparation}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Next departure:</span> {selectedCruise.next_departure}</h3>
-                <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Port:</span> {selectedCruise.port}</h3>
+            <main>
+                <div className='cruise-details'>
+                    <img className='cruise-details__hero' src={cruiseImage} alt='Cruise ship you are seeing the details for' />
+                    <h1 className='cruise-details__h1'>Cruise details</h1>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Destination:</span> {selectedCruise.destination}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Boat name: </span> {selectedCruise.boat_name}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Days at sea:</span> {selectedCruise.days_at_sea}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Capacity:</span> {selectedCruise.capacity}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Kids & family friendly:</span> {selectedCruise.family_friendly}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Last cruise reparation:</span> {selectedCruise.last_cruise_reparation}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Next departure:</span> {selectedCruise.next_departure}</h3>
+                    <h3 className='cruise-details__h3'><span className='cruise-details__bold'>Port:</span> {selectedCruise.port}</h3>
+                </div>
+                <button className='cruise-list__back-button' onClick={handleBackClick}>BACK</button>
             </main>
         )
     }
--- client/src/pages/CruiseList/CruiseList.js
+++ client/src/pages/CruiseList/CruiseList.js
@@ -1,30 +1,56 @@
 import './CruiseList.scss'
 import cruiseList from '../../data/cruise-list.json'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function CruiseList() {
+    const [selectedCruises, setSelectedCruises] = useState();
+
+    function handleCompareClick () {
+        let selectedButton = document.querySelectorAll('.cruise-list__compare-button')  
+
+        if(selectedButton[0].innerHTML === 'COMPARE') {
+            selectedButton[0].innerHTML = 'SELECT CRUISES TO COMPARE'
+        }
+
+        if(selectedButton[0].innerHTML === 'SELECT CRUISES TO COMPARE' && selectedCruises != null ) {
+            console.log(selectedCruises);
+        }
+    }
+
+    function handleSelectCruise (cruiseClick) {
+        console.log(cruiseClick);
+        let selectedButton = document.querySelectorAll('.cruise-list__compare-button')  
+
+        if(selectedButton[0].innerHTML === 'COMPARE') {
+            window.location.replace('/cruise-details/' + cruiseClick)
+        }
+        if(selectedButton[0].innerHTML === 'SELECT CRUISES TO COMPARE') {
+            setSelectedCruises(selectedCruises.append(cruiseClick))
+        }
+    }
+
     return (
         <div className='cruise-list'>
             <h1 className='cruise-list__h1'>Cruise List</h1>
             <section className='cruise-list__compare-container'>
-            <button className='cruise-list__compare-button'>COMPARE</button>
+            <button className='cruise-list__compare-button' onClick={handleCompareClick}>COMPARE</button>
             </section>
-            {cruiseList.map((selectedCruise) => {
+            {cruiseList.map((cruiseIteration) => {
                 return (
-                    <div key={selectedCruise.id} >
-                    <Link className='cruise-list__link' to={'/cruise-details/' + selectedCruise.id}><section className='cruise-list__section'>
+                    <div key={cruiseIteration.id} >
+                    <section onClick={() => {handleSelectCruise(cruiseIteration.id)}} className='cruise-list__section'>
                         <div className='cruise-list__top-container'>
-                            <img className='cruise-list__cruise-photo' src={`http://192.168.3.71:8080/images/bahamas-${selectedCruise.id}`} alt='test' />
+                            <img className='cruise-list__cruise-photo' src={`http://192.168.3.71:8080/images/bahamas-${cruiseIteration.id}`} alt='test' />
                         </div>
                         <div className='cruise-list__bottom-container'>
-                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Boat name:</span> {selectedCruise.boat_name}</h3>
-                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Kids & family friendly:</span> {selectedCruise.family_friendly}</h3>
-                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Next departure:</span> {selectedCruise.next_departure}</h3>
-                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Port:</span> {selectedCruise.port}</h3>
+                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Boat name:</span> {cruiseIteration.boat_name}</h3>
+                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Kids & family friendly:</span> {cruiseIteration.family_friendly}</h3>
+                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Next departure:</span> {cruiseIteration.next_departure}</h3>
+                            <h3 className='cruise-list__h3'><span className='cruise-list__bold'>Port:</span> {cruiseIteration.port}</h3>
                         </div>
                     </section>
-                    </Link>
                     </div>
                 )
             })}
